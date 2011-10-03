d9466daa5a06d4e6f5af5bbac14a2119ac643ec5
Rafael Souza <me@rafaelss.com>
2011-10-03 12:03:55 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YadsTest::Application.config.secret_token = 'b8fa9de22f5462c15473eb6f9f91496d2b55f6de59a90ebae64e1c568842b498b873321f977b2b2b7c7cfe626c1144befae9f7c9daef59c190cdbe93c9112abe'
